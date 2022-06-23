53acdacaac2980e1b8707cf2a278c4fc8902774a
Hector Rios <that.hector@gmail.com>
2022-06-23 00:32:18 -0500

    feat: add users controller + test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,22 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    users = User.all
 
-    render json: @users
+    render json: users
+  end
+
+  def show
+    user = User.find(permitted_params[:id])
+
+    render json: user, status: :ok
+  rescue StandardError
+    render json: {}, status: :unprocessable_entity
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:id)
   end
 end
