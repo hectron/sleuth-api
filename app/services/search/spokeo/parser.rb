29eac418a0f8ca315317dc84b72f67e93829ad63
Hector Rios <that.hector@gmail.com>
2022-07-16 16:45:10 -0500

    DRY up spokeo search
    We found that there's a huge JSON blob embedded in the page which has
    the entire result set, so we just parse that 🙃

--- app/services/search/spokeo/parser.rb
+++ app/services/search/spokeo/parser.rb
@@ -4,8 +4,7 @@
 module Search
   module Spokeo
     class Parser
-      ListingCssClass = ".single-column-list-item"
-      TitleCssClass = ".title"
+      ScriptCssClass = '[data-testid="people-json-ld"]'
 
       def self.parse(html)
         new(html).parse
@@ -15,21 +14,30 @@ module Search
         @html = html
       end
 
+      # rubocop:disable Metrics/AbcSize
       def parse
-        @html_node = Nokogiri::HTML(@html)
-        listing_nodes = @html_node.css(ListingCssClass)
+        html_node = Nokogiri::HTML(@html)
+        results = JSON.parse(html_node.at_css(ScriptCssClass).text)
 
-        listing_nodes.each_with_object([]) do |listing_node, output|
-          title_node = listing_node.at_css(TitleCssClass)
+        results.each_with_object([]) do |listing, output|
+          result = {}
+          result[:name] = listing["name"]
+          result[:alternative_names] = listing["additionalName"]
+          result[:url] = listing["url"]
+          # each relatedTo also has a URL, which would be neat to enable a "unsubscribe" family functionality
+          result[:related_to] = listing["relatedTo"].map { |person| person["name"] }
+          result[:known_addresses] = listing["homeLocation"].map do |place|
+            {
+              city: place.dig("address", "addressLocality"),
+              state: place.dig("address", "addressRegion"),
+              zip: place.dig("address", "postalCode"),
+            }
+          end
 
-          url = "http://www.spokeo.com#{listing_node["href"]}"
-          name_and_age = title_node.text.split(", ")
-          name = name_and_age.first
-          age = name_and_age.second&.to_i
-
-          output.push({ name:, age:, url: })
+          output.push(result)
         end
       end
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
