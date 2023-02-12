c20561c9defbbdfd9da0aaa259cbd5266ed34795
Hector Rios <that.hector@gmail.com>
2023-02-11 19:00:45 -0600

    chore: Add User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create([
+  { first_name: "Lennon", last_name: "Bailey", state: "FL", email: "fakeuser1@gmail.com" },
+  { first_name: "Taylor", last_name: "Ramirez", state: "IL", email: "faketay1@gmail.com" }
+])
